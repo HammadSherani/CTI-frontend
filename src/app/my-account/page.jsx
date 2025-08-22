7f85b9583e8ea97b6dcfd871648bd6420507e7b4
Muhammad Haseeb <muhammadhaseebnarejo@gmail.com>
2025-08-22 15:41:08 +0500

    my account page

---
+++ src/app/my-account/page.jsx
@@ -0,0 +1,12 @@
+import SettingsPage from '@/components/website/SettingsPage'
+import React from 'react'
+
+const Page = () => {
+  return (
+      <div className="bg-gray-50 min-h-screen flex items-center justify-center p-4">
+      <SettingsPage />
+    </div>
+  )
+}
+
+export default Page
