a80f52504c9849475d30bfd00f1c5bf05bb658cb
Hammad Khan <hammad@blinkswag.com>
2025-08-22 23:14:25 +0500

    changes

--- src/app/my-account/page.jsx
+++ src/app/my-account/page.jsx
@@ -1,11 +1,15 @@
 import SettingsPage from '@/components/website/SettingsPage'
 import React from 'react'
+import WebsiteHeader from '@/components/website/Header'
 
 const Page = () => {
   return (
+    <>
+      <WebsiteHeader />
       <div className="bg-gray-50 min-h-screen flex items-center justify-center p-4">
-      <SettingsPage />
-    </div>
+        <SettingsPage />
+      </div>
+    </>
   )
 }
 
