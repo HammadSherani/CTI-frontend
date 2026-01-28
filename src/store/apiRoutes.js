f24a4a9bf3f59cf73095147859a218b7e3782112
Hammad Sherani <hammadk426623@gmail.com>
2026-01-29 01:06:41 +0500

    routes change

--- src/store/apiRoutes.js
+++ src/store/apiRoutes.js
@@ -1,6 +1,6 @@
 // Home Page 
 
-export const HOMEPAGE_API ="/api/home"
+export const HOMEPAGE_API ="/home"
 
 export const GET_ACADEMIC_CATEGORYID_API = "/academic/category"
 export const GET_ACADEMY_BY_CATEGORYID_API = "/academic/category"
